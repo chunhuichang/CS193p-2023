c7a0a6f76d31533f3fda3727aebd41b917a2a36f
Jill Chang <jill20021@gmail.com>
2024-05-10 18:43:46 +0800

    feat: Remove cardCountAdjusters

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,54 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis = ["😈", "👹", "👻", "💀", "👺", "🎃", "🧛🏻‍♂️", "🧟‍♂️", "🕷️", "🦹"]
-    @State var cardCount = 4
 
     var body: some View {
-        VStack {
-            ScrollView {
-                cards
-            }
-            Spacer()
-            cardCountAdjusters
+        ScrollView {
+            cards
         }
         .padding()
     }
 
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
-            ForEach(0 ..< cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index], isFaceUp: true)
                     .aspectRatio(2 / 3, contentMode: .fit)
             }
         }
         .foregroundStyle(.orange)
-    }
-
-    var cardCountAdjusters: some View {
-        HStack {
-            cardReomver
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-
-    var cardReomver: some View {
-        cardCountAdjusters(by: -1, symbol: "minus.circle.fill")
-    }
-
-    var cardAdder: some View {
-        cardCountAdjusters(by: 1, symbol: "plus.circle.fill")
-    }
-
-    func cardCountAdjusters(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
 }
 
