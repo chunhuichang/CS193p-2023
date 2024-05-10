e600d79eec1afadaf41d7e819ca48e62eefa8506
Jill Chang <jill20021@gmail.com>
2024-05-10 18:56:54 +0800

    feat: Create Model and View Model for MemoryGame

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Jill Chang on 2024/5/7.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+//    var model: MemoryGame<String> = MemoryGame()
+}
