87f28699310ce2d4d0a383b7951ed7c9f62ebd02
Jill Chang <jill20021@gmail.com>
2024-05-12 12:58:39 +0800

    feat: Implement init in ViewModel and Model

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,5 +8,19 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-//    var model: MemoryGame<String> = MemoryGame()
+    private static let emojis = ["😈", "👹", "👻", "💀", "👺", "🎃", "🧛🏻‍♂️", "🧟‍♂️", "🕷️", "🦹"]
+
+    private static func createMemoryGame() -> MemoryGame<String> {
+        .init(numberOfPairsOfCards: 4) { pairIndex in
+            emojis.indices.contains(pairIndex) ? emojis[pairIndex] : "⁉️"
+        }
+    }
+
+    private var model: MemoryGame<String> = createMemoryGame()
+
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+
+    func choose(_ card: MemoryGame<String>.Card) {}
 }
