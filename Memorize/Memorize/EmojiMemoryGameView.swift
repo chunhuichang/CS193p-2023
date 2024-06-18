9124530f018b2b9758bd44e0cb1f4006ad1e118d
Jill Chang <jill20021@gmail.com>
2024-06-18 11:17:50 +0800

    feat: Deal cards from a deck

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -15,10 +15,11 @@ struct EmojiMemoryGameView: View {
 
     var body: some View {
         VStack {
-            cards
-                .foregroundStyle(viewModel.color)
+            cards.foregroundStyle(viewModel.color)
             HStack {
                 score
+                Spacer()
+                deck.foregroundStyle(viewModel.color)
                 Spacer()
                 shuffle
             }
@@ -45,14 +46,18 @@ struct EmojiMemoryGameView: View {
 
     private var cards: some View {
         AspectVGrid(viewModel.cards, aspectRatio) { card in
-            CardView(card)
-                .padding(4)
-                .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
-                // FIXME: xcode 15.3 can't display flying text front of the card
-                .zIndex(scoreChange(causedBy: card) != 0 ? 100 : 0)
-                .onTapGesture {
-                    choose(card)
-                }
+            if isDealt(card) {
+                CardView(card)
+                    .padding(4)
+                    .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                    // FIXME: xcode 15.3 can't display flying text front of the card
+                    .zIndex(scoreChange(causedBy: card) != 0 ? 100 : 0)
+                    .onTapGesture {
+                        choose(card)
+                    }
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AsymmetricTransition(insertion: .identity, removal: .identity))
+            }
         }
     }
 
@@ -71,6 +76,47 @@ struct EmojiMemoryGameView: View {
     private func scoreChange(causedBy card: Card) -> Int {
         let (amount, id) = lastScoreChange
         return card.id == id ? amount : 0
+    }
+
+    // MARK: - Dealing from a Deck
+
+    @State private var dealt = Set<Card.ID>()
+
+    private func isDealt(_ card: Card) -> Bool {
+        dealt.contains(card.id)
+    }
+
+    private var undealtCards: [Card] {
+        viewModel.cards.filter { !isDealt($0) }
+    }
+
+    private let deckWidth: CGFloat = 50
+    @Namespace private var dealingNamespace
+
+    private var deck: some View {
+        ZStack {
+            ForEach(undealtCards) { card in
+                CardView(card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AsymmetricTransition(insertion: .identity, removal: .identity))
+            }
+        }
+        .frame(width: deckWidth, height: deckWidth / aspectRatio)
+        .onTapGesture {
+            deal()
+        }
+    }
+
+    private let dealInterval: TimeInterval = 0.15
+    private let dealAnimation: Animation = .easeInOut(duration: 1.0)
+    private func deal() {
+        var delay: TimeInterval = 0
+        for card in viewModel.cards {
+            withAnimation(dealAnimation.delay(delay)) {
+                _ = dealt.insert(card.id)
+            }
+            delay += dealInterval
+        }
     }
 }
 
