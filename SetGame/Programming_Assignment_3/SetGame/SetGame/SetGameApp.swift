e48e58dba03783eb040fcba8003024fccd149c69
Jill Chang <jill20021@gmail.com>
2024-06-24 15:41:17 +0800

    docs: Init A3

---
+++ SetGame/Programming_Assignment_3/SetGame/SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Jill Chang on 2024/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetGame/Programming_Assignment_3/SetGame/SetGameTests/SetGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  SetGameTests.swift
+//  SetGameTests
+//
+//  Created by Jill Chang on 2024/6/24.
+//
+
+import Testing
+@testable import SetGame
+
+struct SetGameTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
